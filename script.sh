0bbca1e2f73270cea3935a7b9cf31ffe329fa66a
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2023-07-14 18:26:38 -0400

    Updated repo

--- script.sh
+++ script.sh
@@ -5,11 +5,6 @@ if [ -f /tmp/gource.log ]; then
 fi
 
 cd py-brain-inventory
-git log --pretty=format:'%h %aN %ad' --date=iso --reverse > /tmp/gource.log
-sed  -E 's/Ivan E. Cao\-Berg/icaoberg|g' /tmp/gource.log 
-sed  -E 's|eduardojfigueroa|eduardo.figueroa7|g' /tmp/gource.log 
+git log --pretty=format:user:%aN%n%ct --reverse --raw > /tmp/gource.log
+gource --log-format custom /tmp/gource.log
 
-#gource --start-date "2023-06-30" --stop-date "2023-08-04" --log-format custom 
-/tmp/gource.log
-
-##gource -o - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 $(date +%Y%m%d).mp4
