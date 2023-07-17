8bd171293adb4812e1d081f54b601a3e9ebed178
Ivan Cao-Berg <icaoberg@psc.edu>
2023-07-16 20:31:02 -0400

    Added basic script

--- script.sh
+++ script.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-if [ -f /tmp/gource.log ]; then
-	rm -f /tmp/gource.log
+if [ -f /tmp/log.txt ]; then
+	rm -f /tmp/log.txt
 fi
 
 cd py-brain-inventory
-git log --pretty=format:user:%aN%n%ct --reverse --raw > /tmp/gource.log
-gource --log-format custom /tmp/gource.log
+git log --pretty=format:user:%aN%n%ct --reverse --raw --encoding=UTF-8 --no-renames --no-show-signature > /tmp/log.txt
+cd ..
 
+#Staff
+sed -i 's|Ivan E. Cao-Berg|Ivan|g' /tmp/log.txt
+sed -i 's|icaoberg|Ivan|g' /tmp/log.txt
+sed -i 's|Ivan Cao-Berg|Ivan|g' /tmp/log.txt
+sed -i 's|eduardo.figueroa7|Eduardo|g' /tmp/log.txt
+sed -i 's|Monica Paz Parra|Monica|g' /tmp/log.txt
+
+#students
+
+gource /tmp/log.txt -s 1 --log-format git --stop-at-end --title "py-brain-inventory" \
+       --user-image-dir images \
+       --logo images/logo.png \
+       -1280x720 \
+       -o - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 \
+       -preset ultrafast -crf 1 -threads 0 -bf 0 output.mp4 \
+       2> /dev/null
+
+if [ -f output.log ]; then
+	rm -f output.log
+fi
